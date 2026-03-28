4fdc43dadeee4757e044b18fae9acfdc8bf3438a
SatsuyaSystems <projectgremory.api@yahoo.com>
2026-03-28 10:43:45 +0100

    Add initial project files and configuration for RuinsCore plugin

---
+++ src/main/java/de/satsuya/ruinsCore/RuinsCore.java
@@ -0,0 +1,17 @@
+package de.satsuya.ruinsCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RuinsCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
